84b7dd1fce58b3fd2d7a7d50f01d6fda07edb38e
kimtaehyeon <kth1927@naver.com>
2024-07-15 16:58:20 +0900

    리팩토링

--- routers/voice_router.py
+++ routers/voice_router.py
@@ -2,7 +2,7 @@ from typing import Dict, List
 
 from fastapi import APIRouter, Form, UploadFile, File
 
-from cruds.voice_crud import fetch_voices, create_voice, voice_on_story
+from cruds.voice_crud import fetch_voices, create_voice, attach_voice_on_story
 router = APIRouter(tags=['voice'])
 
 
@@ -11,10 +11,10 @@ async def get_voices() -> List[Dict[str, str]]:
     voices = fetch_voices()
 
     return [{
+        "id": str(voice["_id"]),
         "name": voice["name"],
         "image": voice["image"],
-        "audio_sample": voice["audio_sample"],
-        "voice_model": voice["voice_model"]
+        "audio": voice["audio_sample"]
     } for voice in voices]
 
 
@@ -31,6 +31,6 @@ async def post_new_voice(name: str = Form(...),
 
 @router.post("/api/stories/{story_id}/voices")
 async def apply_voice_on_story(story_id: str, voice_id: str):
-    voice_on_story(story_id, voice_id)
+    attach_voice_on_story(story_id, voice_id)
 
     return story_id
